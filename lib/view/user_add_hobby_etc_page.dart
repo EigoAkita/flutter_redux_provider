4a70b7df6b3a373f4f270b9985dadec3af82566b
英吾 <mono.eigo@gmail.com>
2021-10-24 15:01:19 +0900

    画面のスワイプの動作の仕様変更完了

--- lib/view/user_add_hobby_etc_page.dart
+++ lib/view/user_add_hobby_etc_page.dart
@@ -30,146 +30,169 @@ class UserAddHobbyEtcPage extends StatelessWidget {
     logger.info("UserAddHobbyEtcPage");
     logger.info(store.state.hobby);
 
-    return Column(
-      children: <Widget>[
-        Expanded(
-          child: SingleChildScrollView(
-            reverse: false,
-            child: Padding(
-              padding: EdgeInsets.only(
-                bottom: _bottomSpace,
-              ),
-              child: GestureDetector(
-                onTap: () {
-                  //TextFieldのフォーカスを正しく外す方法
-                  final FocusScopeNode currentScope = FocusScope.of(context);
-                  if (!currentScope.hasPrimaryFocus && currentScope.hasFocus) {
-                    FocusManager.instance.primaryFocus!.unfocus();
-                  }
-                },
-                child: Container(
-                  width: double.infinity,
-                  color: Colors.grey[300],
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
+    return GestureDetector(
+      onPanUpdate: (DragUpdateDetails details) {
+        if (details.delta.dx > 20) {
+          pageController.animateToPage(
+            _model.store.state.currentIndex! - 1,
+            duration: const Duration(
+              milliseconds: 500,
+            ),
+            curve: Curves.easeInOut,
+          );
+        }
+        if (details.delta.dx < -20 && _model.store.state.hobby!.isNotEmpty) {
+          pageController.animateToPage(
+            _model.store.state.currentIndex! + 1,
+            duration: const Duration(
+              milliseconds: 500,
+            ),
+            curve: Curves.easeInOut,
+          );
+        }
+      },
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: SingleChildScrollView(
+              reverse: false,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  bottom: _bottomSpace,
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    //TextFieldのフォーカスを正しく外す方法
+                    final FocusScopeNode currentScope = FocusScope.of(context);
+                    if (!currentScope.hasPrimaryFocus &&
+                        currentScope.hasFocus) {
+                      FocusManager.instance.primaryFocus!.unfocus();
+                    }
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.grey[300],
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 30),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          SizedBox(
-                            height: 50,
-                          ),
-                          Text('都道府県', style: _titleStyle),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 10,
+                      padding: const EdgeInsets.all(10),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            SizedBox(
+                              height: 50,
                             ),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(5),
+                            Text('都道府県', style: _titleStyle),
+                            SizedBox(
+                              height: 5,
                             ),
-                            child: Consumer<MyAppViewModel>(
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: Consumer<MyAppViewModel>(
+                                builder: (
+                                  _context,
+                                  _model,
+                                  _child,
+                                ) {
+                                  return DropdownButton<String>(
+                                    items: prefectures.map((String value) {
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(value),
+                                      );
+                                    }).toList(),
+                                    isDense: true,
+                                    value: _model.store.state.prefectures,
+                                    onChanged: (value) {
+                                      logger.info(value);
+                                      _model.store
+                                          .dispatch(AddPrefectures(value!));
+                                    },
+                                  );
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              height: 35,
+                            ),
+                            Text('趣味', style: _titleStyle),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 10),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: TextFormField(
+                                controller: _controller,
+                                onChanged: (text) {
+                                  logger.info('${_model.store.state.hobby}');
+                                  _model.store.dispatch(AddHobby(text));
+                                },
+                                minLines: 10,
+                                maxLines: 15,
+                                decoration: InputDecoration.collapsed(
+                                  hintText: '猫と遊ぶ事です！',
+                                  hintStyle: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 2.5,
+                            ),
+                            Consumer<MyAppViewModel>(
                               builder: (
                                 _context,
                                 _model,
                                 _child,
                               ) {
-                                return DropdownButton<String>(
-                                  items: prefectures.map((String value) {
-                                    return DropdownMenuItem<String>(
-                                      value: value,
-                                      child: Text(value),
-                                    );
-                                  }).toList(),
-                                  isDense: true,
-                                  value: _model.store.state.prefectures,
-                                  onChanged: (value) {
-                                    logger.info(value);
-                                    _model.store
-                                        .dispatch(AddPrefectures(value!));
-                                  },
+                                return Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    _model.store.state.isErrorHobby!
+                                        ? Text(
+                                            '${_model.store.state.errorHobby}',
+                                            style: TextStyle(
+                                              fontSize: 10,
+                                              color: Colors.red,
+                                            ),
+                                          )
+                                        : SizedBox(),
+                                    _model.store.state.isErrorHobby!
+                                        ? Text(
+                                            '${_model.store.state.hobby!.length}/500',
+                                            style: TextStyle(
+                                              fontSize: 10,
+                                              color: Colors.red,
+                                            ),
+                                          )
+                                        : Text(
+                                            '${_model.store.state.hobby!.length}/500',
+                                            style: TextStyle(
+                                              fontSize: 10,
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                  ],
                                 );
                               },
                             ),
-                          ),
-                          SizedBox(
-                            height: 35,
-                          ),
-                          Text('趣味', style: _titleStyle),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            child: TextFormField(
-                              controller: _controller,
-                              onChanged: (text) {
-                                logger.info('${_model.store.state.hobby}');
-                                _model.store.dispatch(AddHobby(text));
-                              },
-                              minLines: 10,
-                              maxLines: 15,
-                              decoration: InputDecoration.collapsed(
-                                hintText: '猫と遊ぶ事です！',
-                                hintStyle: TextStyle(
-                                  fontWeight: FontWeight.normal,
-                                ),
-                                border: InputBorder.none,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 2.5,
-                          ),
-                          Consumer<MyAppViewModel>(
-                            builder: (
-                              _context,
-                              _model,
-                              _child,
-                            ) {
-                              return Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  _model.store.state.isErrorHobby!
-                                      ? Text(
-                                          '${_model.store.state.errorHobby}',
-                                          style: TextStyle(
-                                            fontSize: 10,
-                                            color: Colors.red,
-                                          ),
-                                        )
-                                      : SizedBox(),
-                                  _model.store.state.isErrorHobby!
-                                      ? Text(
-                                          '${_model.store.state.hobby!.length}/500',
-                                          style: TextStyle(
-                                            fontSize: 10,
-                                            color: Colors.red,
-                                          ),
-                                        )
-                                      : Text(
-                                          '${_model.store.state.hobby!.length}/500',
-                                          style: TextStyle(
-                                            fontSize: 10,
-                                            color: Colors.grey,
-                                          ),
-                                        ),
-                                ],
-                              );
-                            },
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -177,58 +200,58 @@ class UserAddHobbyEtcPage extends StatelessWidget {
               ),
             ),
           ),
-        ),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              bottom: 50,
-            ),
-            child: TextButton(
-              onPressed: () {
-                if (_model.store.state.hobby!.isEmpty) {
-                  return;
-                } else {
-                  pageController.animateToPage(
-                    _model.store.state.currentIndex! + 1,
-                    duration: const Duration(
-                      milliseconds: 500,
-                    ),
-                    curve: Curves.easeInOut,
-                  );
-                }
-              },
-              child: Consumer<MyAppViewModel>(
-                builder: (
-                  _context,
-                  _model,
-                  _child,
-                ) {
-                  return Container(
-                    width: 150,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      color: _model.store.state.hobby!.isEmpty
-                          ? Colors.grey
-                          : Colors.teal,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Center(
-                      child: Text(
-                        '次へ',
-                        style: TextStyle(
-                          fontSize: 17.5,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                bottom: 50,
+              ),
+              child: TextButton(
+                onPressed: () {
+                  if (_model.store.state.hobby!.isEmpty) {
+                    return;
+                  } else {
+                    pageController.animateToPage(
+                      _model.store.state.currentIndex! + 1,
+                      duration: const Duration(
+                        milliseconds: 500,
+                      ),
+                      curve: Curves.easeInOut,
+                    );
+                  }
+                },
+                child: Consumer<MyAppViewModel>(
+                  builder: (
+                    _context,
+                    _model,
+                    _child,
+                  ) {
+                    return Container(
+                      width: 150,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: _model.store.state.hobby!.isEmpty
+                            ? Colors.grey
+                            : Colors.teal,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '次へ',
+                          style: TextStyle(
+                            fontSize: 17.5,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
